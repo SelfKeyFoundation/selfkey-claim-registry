3753573264978c239e25d8334e434be3de6e0109
Carlos Bruguera <cbruguera@gmail.com>
2019-03-01 10:39:23 +0700

    changed all addresses to bytes32

--- contracts/ClaimRegistry.sol
+++ contracts/ClaimRegistry.sol
@@ -7,26 +7,48 @@ import './interfaces/ERC780.sol';
  *  ERC780 implementation for public claims on (selfkey) identities
  */
 contract ClaimRegistry is ERC780 {
+
+    function toAddress(bytes32 a) internal pure returns (address b) {
+        assembly {
+            mstore(0, a)
+            b := mload(0)
+        }
+        return b;
+    }
+
+    function toBytes32(address a) internal pure returns (bytes32 b) {
+       assembly {
+            mstore(0, a)
+            b := mload(0)
+        }
+       return b;
+    }
+
     constructor() public {
     }
 
     // create or update clams
-    function setClaim(address subject, bytes32 key, bytes32 value) public {
-        registry[msg.sender][subject][key] = value;
-        emit ClaimSet(msg.sender, subject, key, value, now);
+    function setClaim(bytes32 subject, bytes32 key, bytes32 value) public {
+        bytes32 sender = toBytes32(msg.sender);
+        registry[sender][subject][key] = value;
+        emit ClaimSet(sender, subject, key, value, now);
     }
 
     function setSelfClaim(bytes32 key, bytes32 value) public {
-        setClaim(msg.sender, key, value);
+        setClaim(toBytes32(msg.sender), key, value);
     }
 
-    function getClaim(address issuer, address subject, bytes32 key) public view returns(bytes32) {
+    function getClaim(bytes32 issuer, bytes32 subject, bytes32 key) public view returns(bytes32) {
         return registry[issuer][subject][key];
     }
 
-    function removeClaim(address issuer, address subject, bytes32 key) public {
-        require(msg.sender == issuer);
+    function removeClaim(bytes32 issuer, bytes32 subject, bytes32 key) public {
+        require(msg.sender == toAddress(issuer));
         delete registry[issuer][subject][key];
-        emit ClaimRemoved(msg.sender, subject, key, now);
+        emit ClaimRemoved(toBytes32(msg.sender), subject, key, now);
+    }
+
+    function subjectHasClaim(bytes32 issuer, bytes32 subject, bytes32 key) public returns(bool) {
+        return registry[issuer][subject][key] != bytes32(0);
     }
 }
